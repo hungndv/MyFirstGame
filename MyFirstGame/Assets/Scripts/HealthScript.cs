53c9c6f5edaaaf15dede566eadc75aa642d62793
hungndv <hungndv@imt.com.vn>
2014-07-29 17:33:22 +0700

    Hello world game!

---
+++ MyFirstGame/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour
+{
+
+	public int hp = 1;
+	public bool isEnemy = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void Damage (int damageCount)
+	{
+		hp -= damageCount;
+		if (hp <= 0) {
+			SpecialEffectsHelper.Instance.Explosion(transform.position);
+			SoundEffectsHelper.Instance.MakeExplosionSound();
+
+			// Dead!
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D otherCollider)
+	{
+		// Is this a shot?
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> ();
+		if (shot != null) {
+			// Avoid friendly fire
+			if (shot.isEnemyShot != isEnemy) {
+				Damage (shot.damage);
+				// Destroy the shot
+				Destroy (shot.gameObject); // Always remember to target the GameObject,
+				// otherwise you will just remove the script
+			}
+		}
+	}
+}
---
+++ MyFirstGame/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player controller and behaviors.
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2 (50, 50);
+
+	/// <summary>
+	/// 2 - Store the movement
+	/// </summary>
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// 3 - Retrieve asis information
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		// 4 - movement per direction
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+
+		// 5 - shooting
+		bool shoot = Input.GetButtonDown ("Fire1");
+		shoot |= Input.GetButtonDown ("Fire2");
+
+		if (shoot) {
+			WeaponScript weapon = GetComponent<WeaponScript> ();
+			if (weapon != null) {
+				// false because the player is not an enemy
+				weapon.Attack (false);
+				SoundEffectsHelper.Instance.MakePlayerShotSound();
+			}
+		}
+
+		// 6 - Make sure we are not outside the camera bounds
+		var dist = (transform.position - Camera.main.transform.position).z;
+		
+		var leftBorder = Camera.main.ViewportToWorldPoint (
+			new Vector3 (0, 0, dist)
+		).x;
+		
+		var rightBorder = Camera.main.ViewportToWorldPoint (
+			new Vector3 (1, 0, dist)
+		).x;
+		
+		var topBorder = Camera.main.ViewportToWorldPoint (
+			new Vector3 (0, 0, dist)
+		).y;
+		
+		var bottomBorder = Camera.main.ViewportToWorldPoint (
+			new Vector3 (0, 1, dist)
+		).y;
+		
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp (transform.position.y, topBorder, bottomBorder),
+			transform.position.z
+		);
+	}
+
+	void FixedUpdate ()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+
+	void OnDestroy()
+	{
+		// Game Over.
+		// Add the script to the parent because the current game
+		// object is likely going to be destroyed immediately.
+		transform.parent.gameObject.AddComponent<GameOverScript>();
+	}
+}
